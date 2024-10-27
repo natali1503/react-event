8cbe49840564f36565a63652cc80fe6d39404d9b
natali1503 <nfv1503@gmail.com>
2024-10-27 14:26:40 +0500

    add loader data favorites

--- src/components/profile/Favorites.tsx
+++ src/components/profile/Favorites.tsx
@@ -1,21 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppSelector } from '../../hooks/useAppSelector';
 import { getHelpRequests } from '../../store/help-requests/selectors';
 import HelpRequestsComponent from '../HelpRequestsComponent/HelpRequestsComponent';
 import { fetchHelpRequestsAction } from '../../store/api-actions';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
+import { Box, CircularProgress } from '@mui/material';
 
 export default function Favorites() {
   const helpRequestList = useAppSelector(getHelpRequests);
   const dispatch = useAppDispatch();
+  const [isData, setIsData] = useState(helpRequestList.length);
 
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(fetchHelpRequestsAction());
+    setIsData(helpRequestList.length);
   }, []);
 
   return (
     <>
-      <HelpRequestsComponent helpRequests={helpRequestList}/>
+      {!isData ? (
+        <Box display={'flex'} alignItems={'center'} justifyContent={'center'}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <HelpRequestsComponent helpRequests={helpRequestList} />
+      )}
     </>
   );
 }
