81974b957ec0e4a5ae743dd5fefe912ffee4d410
natali1503 <nfv1503@gmail.com>
2024-10-27 16:56:43 +0500

    fix navigate to main

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,26 +5,24 @@ import Link from '@mui/material/Link';
 import { useNavigate } from 'react-router-dom';
 import { AppRoute } from '../../const/const';
 
-
 import LogInButton from './LogInButton';
 import ImageAvatar from './Avatar';
 
 import { useAppSelector } from '../../hooks/useAppSelector';
 
 export default function HeaderNavigationApp() {
-
   const navigate = useNavigate();
-  const isAuthenticated = useAppSelector((store) => store.auth.isAuthenticated)
+  const isAuthenticated = useAppSelector((store) => store.auth.isAuthenticated);
 
   const handleClickRequest = (event: React.MouseEvent<HTMLElement>) => {
     navigate(AppRoute.Main, { replace: true });
   };
 
   return (
-    <Box 
-      sx={{ 
-        backgroundColor: 'white', 
-        height: '84px', 
+    <Box
+      sx={{
+        backgroundColor: 'white',
+        height: '84px',
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
@@ -46,37 +44,44 @@ export default function HeaderNavigationApp() {
           }}
         >
           <img
-            style={{ maxWidth: '220px', height: '40px' }} 
+            style={{ maxWidth: '220px', height: '40px' }}
             srcSet={'/img/LeftSide.svg'}
             src={'/img/LeftSide.svg'}
             alt={'image-title'}
             loading="lazy"
           />
-            <Box 
-                sx={{ 
-                    flexGrow: 1, 
-                    display: 'flex', 
-                    justifyContent: 'center' 
-                }}>
-                <Link onClick={handleClickRequest} color="inherit" href="#" underline="hover"
-                    sx={{ 
-                        fontSize: '16px', 
-                        lineHeight: '150%', 
-                        letterSpacing: '0.15px' 
-                    }}
-                >Запросы о помощи</Link>
-            </Box>
-            <Box 
-                sx={{ 
-                    width: '220px', 
-                    height: '40px', 
-                    display: 'flex', 
-                    justifyContent: 'flex-end', 
-                    alignItems: 'center' 
-                }}
-            > 
-            {isAuthenticated ? <ImageAvatar/> : <LogInButton/>}
-            </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <Link
+              onClick={handleClickRequest}
+              color="inherit"
+              href="#"
+              underline="hover"
+              sx={{
+                fontSize: '16px',
+                lineHeight: '150%',
+                letterSpacing: '0.15px',
+              }}
+            >
+              Запросы о помощи
+            </Link>
+          </Box>
+          <Box
+            sx={{
+              width: '220px',
+              height: '40px',
+              display: 'flex',
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+            }}
+          >
+            {isAuthenticated ? <ImageAvatar /> : <LogInButton />}
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
