903db5d7910ef2faebdb2fd396c7766d6627f709
heyhurricane <dinohka@mail.ru>
2024-10-27 13:26:07 +0400

    fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,9 @@ import { initializeAuth } from './store/authorization';
 import { useAppDispatch } from './hooks/useAppDispatch';
 import './App.css';
 
-import HeaderNavigationApp from './components/Header/Header';
-import BottomNavigationApp from './components/Footer/Footer';
-
-import { Box } from '@mui/material';
 import Wrapper from './components/Wrapper';
 import Profile from './pages/Profile/Profile';
+import Helps from './pages/Helps/Helps';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -36,7 +33,7 @@ function App() {
         <Wrapper>
           <Routes>
             <Route path={AppRoute.Login} element={<LoginPage />} />
-
+            <Route path={AppRoute.Main} element={<Helps />} />
             {/* <Route element={<PrivateRoute />}> */}
             <Route path={AppRoute.Profile} element={<Profile />} />
             {/* </Route> */}
