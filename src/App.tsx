81869d9ff21ec68c97be74af9b0ce9d0d08e6b70
natali1503 <nfv1503@gmail.com>
2024-10-27 11:58:19 +0500

    feat: profile v3

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,10 @@ import { useEffect } from 'react';
 import NotFoundPage from './pages/NotFoundPage';
 import { initializeAuth } from './store/authorization';
 import './App.css';
-import { useAppDispatch } from './hooks/useAppDispatch';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AppRoute } from './const/const';
-import LoginPage from './pages/LoginPage';
-import JustAnotherPage from './pages/JustAnotherPage';
-import PrivateRoute from './components/PrivateRoute';
-import { useEffect } from 'react';
-import NotFoundPage from './pages/NotFoundPage';
-import { initializeAuth } from './store/authorization';
 
 import HeaderNavigationApp from './components/Header/Header';
 import BottomNavigationApp from './components/Footer/Footer';
+import Profile from './components/profile/Profile';
 
 function App() {
   const dispatch = useAppDispatch();
--- src/components/Header/Avatar.tsx
+++ src/components/Header/Avatar.tsx
@@ -3,11 +3,21 @@ import { useState, Fragment } from 'react';
 import { useDispatch } from 'react-redux';
 import { logOut } from '../../store/authorization';
 
-import {Box, Avatar, Menu, MenuItem, ListItemIcon, IconButton} from '@mui/material';
+import {
+  Box,
+  Avatar,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  IconButton,
+} from '@mui/material';
 import Logout from '@mui/icons-material/Logout';
+import { useNavigate } from 'react-router-dom';
+import { AppRoute } from '../../const/const';
 
 export default function ImageAvatar() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
@@ -16,73 +26,75 @@ export default function ImageAvatar() {
     setAnchorEl(event.currentTarget);
   };
 
+  const handleClickProfile = (event: React.MouseEvent<HTMLElement>) => {
+    navigate(AppRoute.Profile, { replace: true });
+  };
   const handleClose = () => {
     dispatch(logOut()); // Вызываем действие для разлогинивания
     setAnchorEl(null);
   };
 
-
-    return (
-        <Fragment>
-        <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-            <IconButton
-              onClick={handleClick}
-              size="small"
-              sx={{ ml: 2 }}
-              aria-controls={open ? 'account-menu' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-            >
-              <Avatar src="/broken-image.jpg" />
-            </IconButton>
-        </Box>
-        <Menu
-          anchorEl={anchorEl}
-          id="account-menu"
-          open={open}
-          onClose={handleClose}
-          onClick={handleClose}
-          slotProps={{
-            paper: {
-              elevation: 0,
-              sx: {
-                overflow: 'visible',
-                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                mt: 1.5,
-                '& .MuiAvatar-root': {
-                  width: 32,
-                  height: 32,
-                  ml: -0.5,
-                  mr: 1,
-                },
-                '&::before': {
-                  content: '""',
-                  display: 'block',
-                  position: 'absolute',
-                  top: 0,
-                  right: 14,
-                  width: 10,
-                  height: 10,
-                  bgcolor: 'background.paper',
-                  transform: 'translateY(-50%) rotate(45deg)',
-                  zIndex: 0,
-                },
+  return (
+    <Fragment>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ ml: 2 }}
+          aria-controls={open ? 'account-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+        >
+          <Avatar src="/broken-image.jpg" />
+        </IconButton>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        slotProps={{
+          paper: {
+            elevation: 0,
+            sx: {
+              overflow: 'visible',
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              mt: 1.5,
+              '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              '&::before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
               },
             },
-          }}
-          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-        >
-          <MenuItem onClick={handleClose}>
-            <Avatar /> Мой профиль
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <ListItemIcon>
-              <Logout fontSize="small" />
-            </ListItemIcon>
-            Выйти
-          </MenuItem>
-        </Menu>
-      </Fragment>
-    );
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem onClick={handleClickProfile}>
+          <Avatar /> Мой профиль
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Выйти
+        </MenuItem>
+      </Menu>
+    </Fragment>
+  );
 }
