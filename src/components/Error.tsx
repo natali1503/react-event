2d930b88b510975df6325077d2b3baa9f8d4820b
natali1503 <nfv1503@gmail.com>
2024-11-13 20:55:57 +0500

    fix page Error

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,10 +1,19 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { ReactSVG } from 'react-svg';
 
 export function Error() {
   return (
-    <Box display={'flex'} justifyContent={'center'}>
+    <Box
+      display={'flex'}
+      justifyContent={'center'}
+      flexDirection={'column'}
+      alignItems={'center'}
+      gap={'20px'}
+    >
       <ReactSVG src="../../public/img/noDataError.svg" />
+      <Typography variant="h5">
+        Ошибка! Не удалось загрузить информацию
+      </Typography>
     </Box>
   );
 }
