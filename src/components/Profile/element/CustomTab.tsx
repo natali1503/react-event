b4949fd17c176b9fc8de5cd51358fd21b55654e8
natali1503 <nfv1503@gmail.com>
2024-12-02 11:58:12 +0500

    tabs in profile have been moved to new component

---
+++ src/components/Profile/element/CustomTab.tsx
@@ -0,0 +1,29 @@
+import { Tab } from '@mui/material';
+import { useMode } from '../../../theme';
+
+export function CustomTab({ label, index, ...props }) {
+  const [theme] = useMode();
+  return (
+    <Tab
+      label={label}
+      {...a11yProps(index)}
+      {...props}
+      sx={{
+        paddingTop: '0px',
+        minHeight: 0,
+        borderBottom: 1,
+        borderColor: 'divider',
+        fontSize: '1.4rem',
+        [`@media (max-width:${theme.breakpoints.values.md}px)`]: {
+          fontSize: '1.2rem',
+        },
+      }}
+    />
+  );
+}
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
---
+++ src/components/Profile/element/TabsProfile.tsx
@@ -0,0 +1,27 @@
+import { Box, Tabs } from '@mui/material';
+import { CustomTab } from './CustomTab';
+export function TabsProfile({ value, setValue }) {
+  function handleChange(e: React.SyntheticEvent, numberTab: number) {
+    setValue(numberTab);
+  }
+
+  return (
+    <Box
+      sx={{
+        padding: '10px 0 0 0',
+      }}
+    >
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        sx={{
+          minHeight: 0,
+        }}
+      >
+        <CustomTab label={'Личные данные'} index={0} />
+        <CustomTab label={'Контакты'} index={1} />
+        <CustomTab label={'Избранное'} index={2} />
+      </Tabs>
+    </Box>
+  );
+}
