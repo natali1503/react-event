2e2269fe720f6410de432cebdee604040c57c96b
natali1503 <nfv1503@gmail.com>
2024-12-02 09:14:29 +0500

    centering middle element in header and footer

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -4,7 +4,6 @@ import { useBreakpointOverlap } from '../../hooks/useBreakpointOverlap';
 
 export function Logo() {
   const { breakpointOverlap } = useBreakpointOverlap();
-  console.log(`${breakpointOverlap ? 'calc(12rem ' : 'calc(31.3rem + 0.11*(100vw - 192rem))'}`);
   return (
     <Box display={'flex'} justifyContent={'flex-start'} alignItems={'center'}>
       <ReactSVG
