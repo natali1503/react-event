fac28019fc2b92b344e51a2cc7a77d0b55eaf6f7
Cinder <148284411+CinerOC@users.noreply.github.com>
2024-12-02 21:42:58 +0300

    [fix] render logic for NotFound and Error components
    - Transfer loading logic for NotFound and Error components into HelpRequestsComponent;
    - Improve UI consistency with minor style adjustments.

--- src/components/HelpRequestsComponent/HelpRequestsComponent.tsx
+++ src/components/HelpRequestsComponent/HelpRequestsComponent.tsx
@@ -4,14 +4,18 @@ import CardList from '../CardList/CardList';
 import { HelpRequest } from '../../types/HelpRequest';
 import MapWrapper from '../Map/MapWrapper';
 import ViewToggle from '../ViewToggle/ViewToggle';
+import { ErrorComponent } from '../Error';
+import { NotFoundResult } from '../NotFoundResult';
 
 type RequestsProps = {
   currentPage: number;
   setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
   helpRequests: HelpRequest[];
+  isHelpRequestsError?: boolean;
+  noSearchResult? : boolean;
 };
 
-const HelpRequestsComponent: FC<RequestsProps> = ({currentPage, setCurrentPage, helpRequests}) => {
+const HelpRequestsComponent: FC<RequestsProps> = ({currentPage, setCurrentPage, helpRequests, noSearchResult, isHelpRequestsError}) => {
   const [viewMode, setViewMode] = useState('grid');
   const itemsPerPage = 3;
   const indexOfLastItem = currentPage * itemsPerPage;
@@ -25,42 +29,59 @@ const HelpRequestsComponent: FC<RequestsProps> = ({currentPage, setCurrentPage, 
   const handleViewChange = useCallback((newViewMode: string) => {
     setViewMode(newViewMode);
   }, []);
- 
+
+  const renderErrorMessage = () => {
+    if (isHelpRequestsError) {
+      return <ErrorComponent/>
+    }
+
+    if (noSearchResult) {
+      return <NotFoundResult />;
+    }
+
+    return null; 
+  };
+
+  const errorMessage = renderErrorMessage();
+
   return (
-    <>
-        <Box sx={{
-          display: 'flex',       
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}>
-          <Typography variant="h6">Найдено: {helpRequests.length}</Typography>
-          <ViewToggle onOptionChange={handleViewChange} />
-        </Box>
-        <Box>
-          {viewMode === 'map' ? (
-            <MapWrapper
-              helpRequests={helpRequests}
-            />
-          ) : (
+    <Box>
+      <Box sx={{
+        display: 'flex',       
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}>
+        <Typography variant="h6">Найдено: {helpRequests.length}</Typography>
+        <ViewToggle onOptionChange={handleViewChange} />
+      </Box>
+      <Box>
+        {errorMessage}
+        {errorMessage === null && (
           <>
-            <CardList helpRequests={ currentItems } viewMode={viewMode}/>
-            <Box sx={{
-              display: 'flex',       
-              justifyContent: 'center',
-              marginTop: '20px',
-            }}>
-              <Pagination
-                count={Math.ceil(helpRequests.length / itemsPerPage)}
-                page={currentPage}
-                size='large'
-                onChange={handlePageChange}
-                color="primary"
-              />
-            </Box>
+            {viewMode === 'map' ? (
+              <MapWrapper helpRequests={helpRequests}/>
+            ) : (
+              <Box>
+                <CardList helpRequests={currentItems} viewMode={viewMode} />
+                <Box sx={{
+                  display: 'flex',       
+                  justifyContent: 'center',
+                  marginTop: '30px'
+                }}>
+                  <Pagination
+                    count={Math.ceil(helpRequests.length / itemsPerPage)}
+                    page={currentPage}
+                    size='large'
+                    onChange={handlePageChange}
+                    color="primary"
+                  />
+                </Box>
+              </Box>
+            )}
           </>
-          )}
-        </Box>
-      </>
+        )}
+      </Box>
+    </Box>
    )
 };
 
