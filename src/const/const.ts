da50186db6b8914d4def7af8d790f8f61792d0f8
Cinder <148284411+CinerOC@users.noreply.github.com>
2025-02-03 19:29:51 +0300

    [fix] change base_url

--- src/const/const.ts
+++ src/const/const.ts
@@ -6,8 +6,8 @@ export enum AppRoute {
   TestPage = '/test', // to delete
 }
 
-//const BASE_URL = 'http://localhost:4040/api';
-export const BASE_URL = 'https://natticharity.eveloth.ru/api';
+const BASE_URL = 'http://localhost:4040/api';
+//export const BASE_URL = 'https://natticharity.eveloth.ru/api';
 
 export enum APIRoute {
   HelpRequests = `${BASE_URL}/request`,
